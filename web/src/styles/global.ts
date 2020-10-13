5089ad1d523766304b21cb02647df9663a13260a
zevdvlpr <zevdvlpr@gmail.com>
2020-10-12 21:19:26 -0300

    :hammer: refactor: add global styles

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -1,5 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    
+    box-sizing: border-box;
+  }
   
+  body {
+    color: #FFF;
+    
+    background: #EBF2F5;
+  }
+  
+  body, input, button, textarea {
+    font: 600 18px Nunito, sans-serif;
+  }
 `;
