81437e8b625e6c4c7f68afd20d8db60eb06ea800
zevdvlpr <zevdvlpr@gmail.com>
2020-10-14 12:26:58 -0300

    :bulb: chore: migrations

---
+++ server/src/database/migrations/1602683169471-createOrphanages.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createOrphanages1602683169471 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'orphanages',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'latitude',
+          type: 'decimal',
+          scale: 7,
+          precision: 10,
+        },
+        {
+          name: 'longitude',
+          type: 'decimal',
+          scale: 7,
+          precision: 10,
+        },
+        {
+          name: 'about',
+          type: 'text',
+        },
+        {
+          name: 'instructions',
+          type: 'text',
+        },
+        
+        {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
+          name: 'open_on_weekends',
+          type: 'boolean',
+          default: false,
+        },
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages');
+  }
+}
+
