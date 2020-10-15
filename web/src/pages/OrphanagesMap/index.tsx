10a9504a0790b8f78020e74fc85f8be49259f384
zevdvlpr <zevdvlpr@gmail.com>
2020-10-15 15:25:46 -0300

    :gear: perf: add API locations

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,49 +1,87 @@
-import React from 'react';	
-import { FiPlus } from 'react-icons/fi';	
-import { Map, TileLayer } from 'react-leaflet';	
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import 'leaflet/dist/leaflet.css';	
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
-import {	
-  Container,	
-  SideBar,	
-  Header,	
-  Footer,	
-  CreateButton,	
-} from './styles';	
+import {
+  Container,
+  SideBar,
+  Header,
+  Footer,
+  CreateButton,
+  ModalPopup,
+} from './styles';
 
-import mapMarkerImg from '../../assets/map-marker.svg';	
+import api from '../../services/api';
 
-const OrphanagesMap: React.FC = () => {	
-  return (	
-    <Container>	
-      <SideBar>	
-        <Header>	
-          <img src={mapMarkerImg} alt="Happy" />	
+import mapMarkerImg from '../../assets/map-marker.svg';
 
-          <h2>Escolha um orfanato no mapa</h2>	
-          <p>Muitas crianças estão esperando a sua visita :)</p>	
-        </Header>	
+import mapIcon from '../../utils/mapIcon';
 
-        <Footer>	
-          <strong>Minas Gerais</strong>	
-          <span>Belo Horizonte</span>	
-        </Footer>	
-      </SideBar>	
+interface Orphanage {
+  id: 12;
+  name: string;
+  latitude: string;
+  longitude: string;
+}
 
-      <Map	
-        center={[-19.7763281, -43.9308896]}	
-        zoom={14}	
-        style={{ width: '100%', height: '100%' }}	
-      >	
-        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />	
-      </Map>	
+const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>();
 
-      <CreateButton to="">	
-        <FiPlus size={32} color="#FFF" />	
-      </CreateButton>	
-    </Container>	
-  );	
-};	
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <SideBar>
+        <Header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </Header>
+
+        <Footer>
+          <strong>Minas Gerais</strong>
+          <span>Belo Horizonte</span>
+        </Footer>
+      </SideBar>
+
+      <Map
+        center={[-19.8612254, -43.9677464]}
+        zoom={14}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+        
+        {orphanages?.map(orphanage => (
+          <Marker
+            key={orphanage.id}
+            icon={mapIcon}
+            position={[Number(orphanage.latitude), Number(orphanage.longitude)]}
+          >
+            <ModalPopup>
+              {orphanage.name}
+
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </ModalPopup>
+          </Marker>
+        ))}        
+      </Map>
+
+      <CreateButton to="/orphanages/create">
+        <FiPlus size={32} color="#FFF" />
+      </CreateButton>
+    </Container>
+  );
+};
 
 export default OrphanagesMap;
--- web/src/pages/OrphanagesMap/styles.ts
+++ web/src/pages/OrphanagesMap/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';	
+import { Popup } from 'react-leaflet';
 import { Link } from 'react-router-dom';	
 
 export const Container = styled.div`	
@@ -62,4 +63,44 @@ export const CreateButton = styled(Link)`
   &:hover {	
     background: #17d6eb;	
   }	
+`;
+
+export const ModalPopup = styled(Popup).attrs({
+  closeButton: false,
+  
+  minWidth: 240,
+  maxWidth: 240,
+})`
+  .leaflet-popup-content-wrapper {
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 20px;
+    box-shadow: none;
+  }
+  
+  .leaflet-popup-content {
+    color: #0089a5;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 8px 12px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .leaflet-popup-content a {
+    width: 40px;
+    height: 40px;
+    background: #15c3d6;
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+    border-radius: 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .leaflet-popup-tip-container {
+    display: none;
+  }
 `;
