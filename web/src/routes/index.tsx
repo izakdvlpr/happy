ffd6a486c812f5f3614a958d432122ade7a25ac8
zevdvlpr <zevdvlpr@gmail.com>
2020-10-15 15:22:46 -0300

    :gear: perf: new routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../pages/Landing';
+import Orphanage from '../pages/Orphanage';
+import CreateOrphanage from '../pages/CreateOrphanage';
 import OrphanagesMap from '../pages/OrphanagesMap';
 
 export default function Routes() {
@@ -9,7 +11,10 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />        
-        <Route path="/app" component={OrphanagesMap} />        
+        <Route path="/app" component={OrphanagesMap} />    
+        
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />    
       </Switch>
     </BrowserRouter>
   )
